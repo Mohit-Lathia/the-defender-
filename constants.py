020998fb0c110b63276681e697e1a00275a9bad2
Mohit-Lathia <114840854+Mohit-Lathia@users.noreply.github.com>
2025-01-14 21:35:32 +0000

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,14 @@
+#TILE_SIZE = 40
+#GRID_WIDTH = 10
+#GRID_HEIGHT = 7
+#SCREEN_WIDTH = TILE_SIZE * GRID_WIDTH
+#SCREEN_HEIGHT = TILE_SIZE * GRID_HEIGHT
+FPS = 60
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+COLORS = {
+    0: (200, 200, 200),  # Empty space (light gray)
+    1: (100, 100, 250),  # Path (blue)
+    2: (0, 255, 0),      # Start point (green)
+    3: (255, 0, 0)       # End point (red)
+}
